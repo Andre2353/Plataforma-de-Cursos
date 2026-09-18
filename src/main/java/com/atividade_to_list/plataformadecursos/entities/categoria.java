ff940ad0c24be18ca664a6653f160e2a29d9dc41
andre2353 <andrepassosdossantos64@gmail.com>
2026-09-17 21:56:26 -0300

    feat(categoria): adiciona novas validacoes e regras em Categoria

--- src/main/java/com/atividade_to_list/plataformadecursos/entities/categoria.java
+++ src/main/java/com/atividade_to_list/plataformadecursos/entities/categoria.java
@@ -1,6 +1,7 @@
 package com.atividade_to_list.plataformadecursos.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,8 @@ public class categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private LocalDateTime dataHora;
 }
