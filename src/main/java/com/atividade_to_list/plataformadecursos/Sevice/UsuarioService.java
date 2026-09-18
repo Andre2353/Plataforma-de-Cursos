bb691da0e571828b30bd1cb2a97aa52921bf25f0
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-18 10:07:58 -0300

    feat: usuario service feita, metodo get feito

--- src/main/java/com/atividade_to_list/plataformadecursos/Sevice/UsuarioService.java
+++ src/main/java/com/atividade_to_list/plataformadecursos/Sevice/UsuarioService.java
@@ -1,9 +1,12 @@
 package com.atividade_to_list.plataformadecursos.Sevice;
 
 import com.atividade_to_list.plataformadecursos.DTOs.UsuarioRequest;
+import com.atividade_to_list.plataformadecursos.DTOs.UsuarioResponse;
 import com.atividade_to_list.plataformadecursos.Repository.UsuarioRepository;
 import com.atividade_to_list.plataformadecursos.entities.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -12,6 +15,7 @@ public class UsuarioService {
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+
     public UsuarioRequest criarUsuario(UsuarioRequest request) {
         Usuario usuario = new Usuario();
         usuario.setName(request.getName());
@@ -19,5 +23,16 @@ public class UsuarioService {
         usuario.setSenha(request.getSenha());
         usuarioRepository.save(usuario);
         return request;
+    }
+
+    public List<UsuarioResponse> mostrarUsuarios() {
+        return usuarioRepository.findAll().stream()
+                .map(usuario -> new UsuarioResponse(
+                        usuario.getId(),
+                        usuario.getName(),
+                        usuario.getEmail(),
+                        usuario.getSenha()
+                ))
+                .toList();
     }
 }
