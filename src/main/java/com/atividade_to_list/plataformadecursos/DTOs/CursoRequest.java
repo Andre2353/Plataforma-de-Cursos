1accdd2c51a4b9125835c7f63680567545b9ac1d
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-18 09:20:48 -0300

    feat(usuario): cria DTO de requisição para Cursos

--- src/main/java/com/atividade_to_list/plataformadecursos/DTOs/CursoRequest.java
+++ src/main/java/com/atividade_to_list/plataformadecursos/DTOs/CursoRequest.java
@@ -1,4 +1,19 @@
 package com.atividade_to_list.plataformadecursos.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CursoRequest {
+    @NotBlank
+    private String titulo;
+    private String Descricao;
+    @NotBlank
+    private String cargahoraria;
 }
--- src/main/java/com/atividade_to_list/plataformadecursos/DTOs/CursoResponse.java
+++ src/main/java/com/atividade_to_list/plataformadecursos/DTOs/CursoResponse.java
@@ -1,4 +1,18 @@
 package com.atividade_to_list.plataformadecursos.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CursoResponse {
+    private String titulo;
+    private String Descricao;
+    @NotBlank
+    private String cargahoraria;
 }
