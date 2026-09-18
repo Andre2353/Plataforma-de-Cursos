1cd68396c09d72dd34249be2d828311b0f80a77a
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-18 11:03:41 -0300

    feat: Curso Controlle

---
+++ src/main/java/com/atividade_to_list/plataformadecursos/controller/cursoController.java
@@ -0,0 +1,54 @@
+package com.atividade_to_list.plataformadecursos.controller;
+
+import com.atividade_to_list.plataformadecursos.DTOs.CursoRequest;
+import com.atividade_to_list.plataformadecursos.DTOs.CursoResponse;
+import com.atividade_to_list.plataformadecursos.Sevice.CursoService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("curso")
+public class cursoController {
+
+    private final CursoService cursoService;
+
+    public cursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CursoResponse> criarCurso(@Valid @RequestBody CursoRequest request) {
+        CursoResponse novoCurso = cursoService.criarCurso(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCurso);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CursoResponse>> mostrarCursos() {
+        List<CursoResponse> cursos = cursoService.mostrarcursos();
+        return ResponseEntity.ok(cursos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CursoResponse> buscarPorId(@PathVariable Long id) {
+        CursoResponse curso = cursoService.buscarpoid(id);
+        return ResponseEntity.ok(curso);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CursoResponse> atualizarCurso(@PathVariable Long id, @Valid @RequestBody CursoRequest request) {
+        CursoResponse cursoAtualizado = cursoService.atualizarid(id, request);
+        return ResponseEntity.ok(cursoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCurso(@PathVariable Long id) {
+        cursoService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
