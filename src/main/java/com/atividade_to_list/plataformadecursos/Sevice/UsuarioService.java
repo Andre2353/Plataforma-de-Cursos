f4edd218c1045d34efce65a1538dd3c9a3f2ea37
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-18 10:09:42 -0300

    feat: usuario service feita, metodo put feito

--- src/main/java/com/atividade_to_list/plataformadecursos/Sevice/UsuarioService.java
+++ src/main/java/com/atividade_to_list/plataformadecursos/Sevice/UsuarioService.java
@@ -45,4 +45,15 @@ public class UsuarioService {
             return "Usuário kickado";
         }
     }
+    public String atualizarid(Long id, Usuario usuarioatualizado) {
+        Usuario usuarioexistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("usuario não encontrado: " + id));
+
+        usuarioexistente.setName(usuarioatualizado.getName());
+        usuarioexistente.setEmail(usuarioatualizado.getEmail());
+        usuarioexistente.setSenha(usuarioatualizado.getSenha());
+
+        usuarioRepository.save(usuarioexistente);
+        return "usuario atualizado com sucesso";
+    }
 }
